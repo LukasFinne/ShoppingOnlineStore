540e1fdd2c7126902e730053b1f72bbb1386a7db
Lukas <lukas.ikvm1@gmail.com>
2022-09-14 13:04:16 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import LogIn from "./components/Log-in.vue";
+</script>
+
+<template>
+  <header>
+    <h2>ShoppingGreen@Us</h2>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+      <LogIn />
+
+      <p>Cart</p>
+    </nav>
+  </header>
+
+  <footer>
+    <nav>
+      <a href="#">Instagram</a>
+      <a href="#">Facebook</a>
+      <p>Contact us: shopping_green@Us.com</p>
+    </nav>
+  </footer>
+  <RouterView />
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  padding: 2em;
+  max-width: 100%;
+  max-height: 5%;
+  justify-content: space-evenly;
+  align-items: center;
+  border-bottom: 1px solid;
+}
+
+header h2 {
+  max-width: 15%;
+  max-height: 10%;
+  padding: 0.5em;
+  border-radius: 1em;
+  background-color: #50c878;
+  color: white;
+}
+
+header p {
+  margin: 0;
+}
+
+header nav {
+  display: flex;
+  flex-grow: 2;
+  max-width: 80%;
+  justify-content: space-evenly;
+  align-items: baseline;
+}
+
+footer nav {
+  display: flex;
+  justify-content: space-evenly;
+  align-items: baseline;
+}
+</style>
+
+<style>
+body {
+  background-color: white;
+}
+</style>
---
+++ src/components/Log-in.vue
@@ -0,0 +1,64 @@
+<script setup>
+import { ref } from "vue";
+
+const showMenu = ref(false);
+
+let borderSize = ref("0.5em");
+</script>
+
+<template>
+  <div>
+    <div
+      id="login"
+      @click="showMenu = !showMenu"
+      v-bind:style="[
+        showMenu
+          ? {
+              borderTopLeftRadius: borderSize,
+              borderTopRightRadius: borderSize,
+            }
+          : {
+              borderTopLeftRadius: borderSize,
+              borderTopRightRadius: borderSize,
+              borderBottomLeftRadius: borderSize,
+              borderBottomRightRadius: borderSize,
+            },
+      ]"
+    >
+      Login
+    </div>
+
+    <form v-if="showMenu" action="#">
+      <label for="uname"><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required />
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required />
+
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+#login {
+  padding: 0.5em;
+  background-color: #0e0e10;
+  color: white;
+  text-align: center;
+  cursor: pointer;
+}
+
+form {
+  display: flex;
+  position: absolute;
+  flex-direction: column;
+  padding: 1em;
+  background-color: #50c878;
+  border-radius: 0em 0.5em 0.5em 0.5em;
+}
+
+form button {
+  margin-top: 0.5em;
+}
+</style>
