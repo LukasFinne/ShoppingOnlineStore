47bce2c09bfbc527ae01ad701b3b99d7c390a3a3
Lukas Finne <lukas.ikvm1@gmail.com>
2022-09-21 13:05:46 +0200

    Add about us information

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,54 @@
 <template>
-  <h2>ABOUT US</h2>
+  <h1>ABOUT US</h1>
+
+  <div class="container">
+    <h3>Made and sold by us</h3>
+    <p>
+      Save money by shopping for ShoppingGreen house brand products made and
+      sold directly by us - not third-party sellers.
+    </p>
+
+    <h3>Premium quality</h3>
+
+    <p>
+      All ShoppingGreen Brands meet strict standards and Current Good
+      Manufacturing Practices set by the FDA. We use only well-researched raw
+      materials, unique ingredients, and trusted blends.
+    </p>
+
+    <h3>90-day, money-back</h3>
+
+    <p>
+      guarantee We're so confident in our ShoppingGreen brands that we offer a
+      full refund if you don’t love our products.
+    </p>
+  </div>
 </template>
+
+<style scoped>
+h1 {
+  border-radius: 0.5em;
+  text-align: center;
+  background-color: #50c878;
+  color: white;
+  box-shadow: rgba(99, 99, 99, 0.2) 0em 0.125em 0.5em 0em;
+}
+
+h3 {
+  margin: 0;
+}
+
+.container {
+  max-width: 80%;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  background-color: #50c878;
+  border-radius: 0.5em;
+  font-size: 1.5em;
+  color: white;
+  padding: 1em;
+  margin-bottom: 1em;
+  box-shadow: rgba(99, 99, 99, 0.2) 0em 0.125em 0.5em 0em;
+}
+</style>
